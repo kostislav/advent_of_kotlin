1be5c2aa16ec123b4db67b46891d487066da7281
Jan Judas <jan@judas.cz>
2023-11-25 20:18:23 +0100

    just use lists

--- src/main/kotlin/cz/judas/jan/advent/helpers.kt
+++ src/main/kotlin/cz/judas/jan/advent/helpers.kt
@@ -3,60 +3,34 @@ package cz.judas.jan.advent
 import com.google.common.collect.Ordering
 import java.io.InputStream
 import java.nio.charset.StandardCharsets
-import kotlin.experimental.ExperimentalTypeInference
 
-fun <I, O> Iterator<I>.map(transformation: (I) -> O): Iterator<O> {
-    val original = this
-
-    return object : Iterator<O> {
-        override fun hasNext(): Boolean = original.hasNext()
-
-        override fun next(): O = transformation(original.next())
-    }
-}
-
-fun Iterator<Int>.sum() : Int {
-    return asSequence().sum()
-}
-
-fun <T : Comparable<T>> Iterator<T>.max() : T {
-    return asSequence().maxOrNull()!!
-}
-
-fun <T : Comparable<T>> Iterator<T>.maxN(howMany: Int) : List<T> {
+fun <T : Comparable<T>> List<T>.maxN(howMany: Int): List<T> {
     return Ordering.natural<T>().greatestOf(this, howMany)
 }
 
-@OptIn(ExperimentalTypeInference::class)
-fun <T> generatedIterator(@BuilderInference block: suspend SequenceScope<T>.() -> Unit): Iterator<T> {
-    val s = Sequence { iterator(block) }
-    return s.iterator()
-}
+fun <T> List<T>.splitOn(predicate: (T) -> Boolean): List<List<T>> {
+    val result = mutableListOf<List<T>>()
+    val current = mutableListOf<T>()
 
-fun <T> Iterator<T>.splitOn(predicate: (T) -> Boolean): Iterator<Iterator<T>> {
-    val original = this
-
-    return generatedIterator {
-        while (original.hasNext()) {
-            yield(generatedIterator inner@{
-                while (original.hasNext()) {
-                    val next = original.next()
-                    if (predicate(next)) {
-                        return@inner
-                    } else {
-                        yield(next)
-                    }
-                }
-            })
+    for (item in this) {
+        if (predicate(item)) {
+            if (!current.isEmpty()) {
+                result += current.toList()
+            }
+            current.clear()
+        } else {
+            current += item
         }
     }
+
+    return result
 }
 
 class InputData(private val source: () -> InputStream) {
-    fun <T> transformLines(transformation: (Iterator<String>) -> T): T {
+    fun <T> transformLines(transformation: (List<String>) -> T): T {
         return source()
             .reader(StandardCharsets.UTF_8)
-            .useLines { transformation(it.iterator()) }
+            .useLines { transformation(it.toList()) }
     }
 
     companion object {
