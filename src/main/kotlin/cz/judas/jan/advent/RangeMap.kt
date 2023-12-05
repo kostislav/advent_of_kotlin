86e11aab958a20d60f5140556ba246713a0e8f56
Jan Judas <jan@judas.cz>
2023-12-05 20:40:01 +0100

    move RangeMap

---
+++ src/main/kotlin/cz/judas/jan/advent/RangeMap.kt
@@ -0,0 +1,59 @@
+package cz.judas.jan.advent
+
+import com.google.common.collect.Ordering
+import java.util.*
+
+class RangeMap<K : Comparable<K>, V>(
+    private val values: NavigableMap<K, Pair<K, V>>,
+    private val defaultValue: V
+) {
+    operator fun get(key: K): V {
+        val possibleRange = values.floorEntry(key)?.value
+        return if (possibleRange === null || possibleRange.first < key) {
+            defaultValue
+        } else {
+            possibleRange.second
+        }
+    }
+
+    fun entriesFor(range: BetterRange<K>): List<Pair<BetterRange<K>, V>> {
+        val result = mutableListOf<Pair<BetterRange<K>, V>>()
+        var current = range.start
+        while (current < range.endExclusive) {
+            val lower = values.floorEntry(current)
+            if (lower === null || lower.value.first < current) {
+                val nextStart = values.ceilingKey(current) ?: range.endExclusive
+                result += BetterRange(current, nextStart) to defaultValue
+                current = nextStart
+            } else {
+                if (current < lower.value.first) {
+                    val nextStart = Ordering.natural<K>().min(lower.value.first, range.endExclusive)
+                    result += BetterRange(current, nextStart) to lower.value.second
+                    current = nextStart
+                } else {
+                    val nextStart = values.ceilingKey(current) ?: range.endExclusive
+                    result += BetterRange(current, nextStart) to defaultValue
+                    current = nextStart
+                }
+            }
+        }
+        return result
+    }
+
+    companion object {
+        fun <K : Comparable<K>, V> fromPairs(
+            pairs: Iterable<Pair<BetterRange<K>, V>>,
+            defaultValue: V
+        ): RangeMap<K, V> {
+            val mapping = TreeMap<K, Pair<K, V>>()
+            pairs.forEach { (range, value) -> mapping.put(range.start, range.endExclusive to value) }
+            return RangeMap(mapping, defaultValue)
+        }
+    }
+}
+
+data class BetterRange<T>(val start: T, val endExclusive: T)
+
+fun BetterRange<Long>.offsetBy(delta: Long): BetterRange<Long> {
+    return BetterRange(start + delta, endExclusive + delta)
+}
--- src/main/kotlin/cz/judas/jan/advent/year2023/Day5.kt
+++ src/main/kotlin/cz/judas/jan/advent/year2023/Day5.kt
@@ -1,11 +1,12 @@
 package cz.judas.jan.advent.year2023
 
-import com.google.common.collect.Ordering
+import cz.judas.jan.advent.BetterRange
 import cz.judas.jan.advent.InputData
 import cz.judas.jan.advent.Pattern
+import cz.judas.jan.advent.RangeMap
 import cz.judas.jan.advent.SplitOn
+import cz.judas.jan.advent.offsetBy
 import cz.judas.jan.advent.parserFor
-import java.util.*
 
 object Day5 {
     fun part1(input: InputData): Long {
@@ -84,61 +85,6 @@ object Day5 {
         operator fun get(range: BetterRange<Long>): List<BetterRange<Long>> {
             return mapping.entriesFor(range)
                 .map { it.first.offsetBy(it.second) }
-        }
-    }
-
-    data class BetterRange<T>(val start: T, val endExclusive: T)
-
-    fun BetterRange<Long>.offsetBy(delta: Long): BetterRange<Long> {
-        return BetterRange(start + delta, endExclusive + delta)
-    }
-
-    class RangeMap<K : Comparable<K>, V>(
-        private val values: NavigableMap<K, Pair<K, V>>,
-        private val defaultValue: V
-    ) {
-        operator fun get(key: K): V {
-            val possibleRange = values.floorEntry(key)?.value
-            return if (possibleRange === null || possibleRange.first < key) {
-                defaultValue
-            } else {
-                possibleRange.second
-            }
-        }
-
-        fun entriesFor(range: BetterRange<K>): List<Pair<BetterRange<K>, V>> {
-            val result = mutableListOf<Pair<BetterRange<K>, V>>()
-            var current = range.start
-            while (current < range.endExclusive) {
-                val lower = values.floorEntry(current)
-                if (lower === null || lower.value.first < current) {
-                    val nextStart = values.ceilingKey(current) ?: range.endExclusive
-                    result += BetterRange(current, nextStart) to defaultValue
-                    current = nextStart
-                } else {
-                    if (current < lower.value.first) {
-                        val nextStart = Ordering.natural<K>().min(lower.value.first, range.endExclusive)
-                        result += BetterRange(current, nextStart) to lower.value.second
-                        current = nextStart
-                    } else {
-                        val nextStart = values.ceilingKey(current) ?: range.endExclusive
-                        result += BetterRange(current, nextStart) to defaultValue
-                        current = nextStart
-                    }
-                }
-            }
-            return result
-        }
-
-        companion object {
-            fun <K : Comparable<K>, V> fromPairs(
-                pairs: Iterable<Pair<BetterRange<K>, V>>,
-                defaultValue: V
-            ): RangeMap<K, V> {
-                val mapping = TreeMap<K, Pair<K, V>>()
-                pairs.forEach { (range, value) -> mapping.put(range.start, range.endExclusive to value) }
-                return RangeMap(mapping, defaultValue)
-            }
         }
     }
 }
