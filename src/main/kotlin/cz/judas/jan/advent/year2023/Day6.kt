387b070ca2bd578156abf9de9f28e1a88a238e61
Jan Judas <jan@judas.cz>
2023-12-06 07:12:21 +0100

    day 6 optimal

--- src/main/kotlin/cz/judas/jan/advent/year2023/Day6.kt
+++ src/main/kotlin/cz/judas/jan/advent/year2023/Day6.kt
@@ -1,34 +1,55 @@
 package cz.judas.jan.advent.year2023
 
+import cz.judas.jan.advent.Answer
 import cz.judas.jan.advent.InputData
 import cz.judas.jan.advent.Pattern
 import cz.judas.jan.advent.SplitOnPattern
 import cz.judas.jan.advent.parserFor
 import cz.judas.jan.advent.product
 import cz.judas.jan.advent.splitOnOnly
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 object Day6 {
+    @Answer("4403592")
     fun part1(input: InputData): Int {
         return parserFor<RaceTable>().parse(input.asString())
             .games()
-            .map { game ->
-                (1..<game.time)
-                    .map { it * (game.time - it) }
-                    .filter { it > game.distance }
-                    .size
-            }
+            .map { game -> waysToWin(game.time.toLong(), game.distance.toLong()) }
             .product()
     }
 
+    @Answer("38017587")
     fun part2(input: InputData): Int {
         val (time, distance) = input.lines()
-            .map { it.splitOnOnly(":").second.filterNot { it == ' ' }.toLong() }
+            .map { line -> line.splitOnOnly(":").second.filterNot { it == ' ' }.toLong() }
 
-        return (1..<time)
-            .asSequence()
-            .map { it * (time - it) }
-            .filter { it > distance }
-            .count()
+        return waysToWin(time, distance)
+    }
+
+    private fun waysToWin(time: Long, maxDistance: Long): Int {
+        val inflectionPoint = time / 2.0
+        val limit = sqrt(inflectionPoint * inflectionPoint - maxDistance)
+        return nextSmallerInt(inflectionPoint + limit) - nextBiggerInt(inflectionPoint - limit) + 1
+    }
+
+    fun nextSmallerInt(value: Double): Int {
+        val floor = floor(value)
+        return if (floor == value) {
+            floor - 1
+        } else {
+            floor
+        }.toInt()
+    }
+
+    fun nextBiggerInt(value: Double): Int {
+        val ceil = ceil(value)
+        return if (ceil == value) {
+            ceil + 1
+        } else {
+            ceil
+        }.toInt()
     }
 
     @Pattern("(.*)\n(.*)")
