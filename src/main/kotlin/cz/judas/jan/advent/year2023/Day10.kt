3d0bbb77e503276c6a7844b77b253c097b4d57ad
Jan Judas <jan@judas.cz>
2023-12-10 11:43:45 +0100

    day 10 so ugly it hurts

--- src/main/kotlin/cz/judas/jan/advent/year2023/Day10.kt
+++ src/main/kotlin/cz/judas/jan/advent/year2023/Day10.kt
@@ -3,27 +3,25 @@ package cz.judas.jan.advent.year2023
 import cz.judas.jan.advent.Answer
 import cz.judas.jan.advent.InputData
 import cz.judas.jan.advent.TwoDimensionalArray
-import cz.judas.jan.advent.customParser
-import cz.judas.jan.advent.map
 
 object Day10 {
+    private val pipeTypes = mapOf(
+        '-' to setOf(Direction.LEFT, Direction.RIGHT),
+        '|' to setOf(Direction.UP, Direction.DOWN),
+        'L' to setOf(Direction.UP, Direction.RIGHT),
+        'J' to setOf(Direction.UP, Direction.LEFT),
+        '7' to setOf(Direction.DOWN, Direction.LEFT),
+        'F' to setOf(Direction.DOWN, Direction.RIGHT),
+    )
+    private val movement = mapOf(
+        Direction.UP to Pair(-1, 0),
+        Direction.DOWN to Pair(1, 0),
+        Direction.LEFT to Pair(0, -1),
+        Direction.RIGHT to Pair(0, 1),
+    )
+
     @Answer("6842")
     fun part1(input: InputData): Int {
-        val pipeTypes = mapOf(
-            '-' to setOf(Direction.LEFT, Direction.RIGHT),
-            '|' to setOf(Direction.UP, Direction.DOWN),
-            'L' to setOf(Direction.UP, Direction.RIGHT),
-            'J' to setOf(Direction.UP, Direction.LEFT),
-            '7' to setOf(Direction.DOWN, Direction.LEFT),
-            'F' to setOf(Direction.DOWN, Direction.RIGHT),
-        )
-        val movement = mapOf(
-            Direction.UP to Pair(-1, 0),
-            Direction.DOWN to Pair(1, 0),
-            Direction.LEFT to Pair(0, -1),
-            Direction.RIGHT to Pair(0, 1),
-        )
-
         val diagram = TwoDimensionalArray.charsFromLines(input.lines())
         val startingPosition = (0..<diagram.numRows).asSequence()
             .flatMap { row -> (0..<diagram.numColumns).asSequence().map { Pair(row, it) } }
@@ -32,8 +30,8 @@ object Day10 {
         val startingDirections = Direction.entries
             .filter {
                 val nextPosition = startingPosition + movement.getValue(it)
-                val field = diagram.getOrNull(nextPosition.first, nextPosition.second)?:'.'
-                it in (pipeTypes[field]?:emptySet()).map { it.inverse() }
+                val field = diagram.getOrNull(nextPosition.first, nextPosition.second) ?: '.'
+                it in (pipeTypes[field] ?: emptySet()).map { it.inverse() }
             }
 
         var length = 1
@@ -48,8 +46,199 @@ object Day10 {
         return length / 2
     }
 
+    @Answer("393")
     fun part2(input: InputData): Int {
-        return 0
+        val diagram = TwoDimensionalArray.charsFromLines(input.lines())
+        val startingPosition = (0..<diagram.numRows).asSequence()
+            .flatMap { row -> (0..<diagram.numColumns).asSequence().map { Pair(row, it) } }
+            .first { diagram[it.first, it.second] == 'S' }
+
+        val startingDirections = Direction.entries
+            .filter {
+                val nextPosition = startingPosition + movement.getValue(it)
+                val field = diagram.getOrNull(nextPosition.first, nextPosition.second) ?: '.'
+                it in (pipeTypes[field] ?: emptySet()).map { it.inverse() }
+            }
+
+        val analyzed = Mutable2dArray<FieldType?>(diagram.numRows, diagram.numColumns, null)
+        var currentDirection = startingDirections[0]
+        var currentPosition = startingPosition + movement.getValue(currentDirection)
+        var rightBias = 0
+        while (currentPosition != startingPosition) {
+            val previousDirection = currentDirection
+            currentDirection = pipeTypes.getValue(diagram[currentPosition])
+                .first { it != currentDirection.inverse() }
+
+            when (previousDirection) {
+                Direction.UP ->
+                    when (currentDirection) {
+                        Direction.UP -> {
+                            updateField(analyzed, currentPosition, 0, -1, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, 0, 1, FieldType.RIGH)
+                        }
+
+                        Direction.RIGHT -> {
+                            updateField(analyzed, currentPosition, 0, -1, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, -1, -1, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, -1, 0, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, +1, 1, FieldType.RIGH)
+                            rightBias += 1
+                        }
+
+                        Direction.LEFT -> {
+                            updateField(analyzed, currentPosition, 0, 1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, -1, 1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, -1, 0, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 1, -1, FieldType.LEFT)
+                            rightBias -= 1
+                        }
+
+                        else -> {}
+                    }
+
+                Direction.DOWN -> {
+                    when (currentDirection) {
+                        Direction.DOWN -> {
+                            updateField(analyzed, currentPosition, 0, 1, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, 0, -1, FieldType.RIGH)
+                        }
+
+                        Direction.RIGHT -> {
+                            updateField(analyzed, currentPosition, 0, -1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 1, -1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 1, 0, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, -1, 1, FieldType.LEFT)
+                            rightBias -= 1
+                        }
+
+                        Direction.LEFT -> {
+                            updateField(analyzed, currentPosition, -1, -1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 1, 0, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, 1, 1, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, 0, 1, FieldType.LEFT)
+                            rightBias += 1
+                        }
+
+                        else -> {}
+                    }
+                }
+
+                Direction.LEFT -> {
+                    when (currentDirection) {
+                        Direction.LEFT -> {
+                            updateField(analyzed, currentPosition, -1, 0, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 1, 0, FieldType.LEFT)
+                        }
+                        Direction.UP -> {
+                            updateField(analyzed, currentPosition, -1, 1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 1, 0, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, 1, -1, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, 0, -1, FieldType.LEFT)
+                            rightBias += 1
+                        }
+                        Direction.DOWN -> {
+                            updateField(analyzed, currentPosition, -1, 0, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, -1, -1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 0, -1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 1, 1, FieldType.LEFT)
+                            rightBias -= 1
+                        }
+                        else -> {}
+                    }
+                }
+
+                Direction.RIGHT -> {
+                    when (currentDirection) {
+                        Direction.RIGHT -> {
+                            updateField(analyzed, currentPosition, 1, 0, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, -1, 0, FieldType.LEFT)
+                        }
+                        Direction.UP -> {
+                            updateField(analyzed, currentPosition, 1, 0, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 1, 1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, 0, 1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, -1, -1, FieldType.LEFT)
+                            rightBias -= 1
+                        }
+                        Direction.DOWN -> {
+                            updateField(analyzed, currentPosition, 1, -1, FieldType.RIGH)
+                            updateField(analyzed, currentPosition, -1, 0, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, -1, 1, FieldType.LEFT)
+                            updateField(analyzed, currentPosition, 0, 1, FieldType.LEFT)
+                            rightBias += 1
+                        }
+                        else -> {}
+                    }
+                }
+            }
+
+            currentPosition += movement.getValue(currentDirection)
+            analyzed.set(currentPosition.first, currentPosition.second, FieldType.LOOP)
+        }
+        val lookingFor = if (rightBias > 0) FieldType.RIGH else FieldType.LEFT
+        while (true) {
+            var foundMore = false
+            var foundCount = 0
+            for (row in 0..<analyzed.numRows) {
+                for (column in 0..<analyzed.numColumns) {
+                    if (analyzed.getSafe(row, column) == lookingFor) {
+                        foundCount += 1
+                        if (analyzed.isInside(row - 1, column - 1) && analyzed.getSafe(row - 1, column - 1) === null) {
+                            foundMore = true
+                            analyzed.set(row - 1, column - 1, lookingFor)
+                        }
+                        if (analyzed.isInside(row - 1, column) && analyzed.getSafe(row - 1, column) === null) {
+                            foundMore = true
+                            analyzed.set(row - 1, column, lookingFor)
+                        }
+                        if (analyzed.isInside(row - 1, column + 1) && analyzed.getSafe(row - 1, column + 1) === null) {
+                            foundMore = true
+                            analyzed.set(row - 1, column + 1, lookingFor)
+                        }
+                        if (analyzed.isInside(row, column - 1) && analyzed.getSafe(row, column - 1) === null) {
+                            foundMore = true
+                            analyzed.set(row, column - 1, lookingFor)
+                        }
+                        if (analyzed.isInside(row, column + 1) && analyzed.getSafe(row, column + 1) === null) {
+                            foundMore = true
+                            analyzed.set(row, column + 1, lookingFor)
+                        }
+                        if (analyzed.isInside(row + 1, column - 1) && analyzed.getSafe(row + 1, column - 1) === null) {
+                            foundMore = true
+                            analyzed.set(row + 1, column - 1, lookingFor)
+                        }
+                        if (analyzed.isInside(row + 1, column) && analyzed.getSafe(row + 1, column) === null) {
+                            foundMore = true
+                            analyzed.set(row + 1, column, lookingFor)
+                        }
+                        if (analyzed.isInside(row + 1, column + 1) && analyzed.getSafe(row + 1, column + 1) === null) {
+                            foundMore = true
+                            analyzed.set(row + 1, column + 1, lookingFor)
+                        }
+                    }
+                }
+            }
+            if (!foundMore) {
+                return foundCount
+            }
+        }
+//        analyzed.items.forEach { println(it) } // TODO
+
+//        return 0
+    }
+
+    fun updateField(
+        field: Mutable2dArray<FieldType?>,
+        currentPosition: Pair<Int, Int>,
+        rowOffset: Int,
+        columnOffset: Int,
+        value: FieldType
+    ) {
+        val targetRow = currentPosition.first + rowOffset
+        val targetColumn = currentPosition.second + columnOffset
+        if (field.getSafe(targetRow, targetColumn) != FieldType.LOOP) {
+            field.setSafe(targetRow, targetColumn, value)
+        }
     }
 
     operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> {
@@ -65,6 +254,53 @@ object Day10 {
                 DOWN -> UP
                 LEFT -> RIGHT
                 RIGHT -> LEFT
+            }
+        }
+    }
+
+    enum class FieldType {
+        LOOP, LEFT, RIGH  /* TODO */
+    }
+
+    class Mutable2dArray<T> private constructor(/*private TODO*/ val items: MutableList<MutableList<T>>,
+                                                private val defaultValue: T
+    ) {
+        val numRows get() = items.size
+
+        val numColumns get() = items[0].size
+
+        fun set(row: Int, column: Int, value: T) {
+            items[row][column] = value
+        }
+
+        fun setSafe(row: Int, column: Int, value: T) {
+            if (isInside(row, column)) {
+                items[row][column] = value
+            }
+        }
+
+        fun getSafe(row: Int, column: Int): T {
+            return if (isInside(row, column)) {
+                items[row][column]
+            } else {
+                defaultValue
+            }
+        }
+
+        fun isInside(row: Int, column: Int): Boolean {
+            return row in 0..<numRows && column in 0..<numColumns
+        }
+
+        companion object {
+            operator fun <T> invoke(numRows: Int, numColumns: Int, defaultValue: T): Mutable2dArray<T> {
+                return Mutable2dArray(
+                    MutableList(numRows) { i ->
+                        MutableList(numColumns) { j ->
+                            defaultValue
+                        }
+                    },
+                    defaultValue
+                )
             }
         }
     }
