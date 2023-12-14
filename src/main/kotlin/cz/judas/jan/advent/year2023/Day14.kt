51fe891346ad8ec182b3b220e80c737ce628bb5e
Jan Judas <jan@judas.cz>
2023-12-14 08:26:11 +0100

    slight deduplication

--- src/main/kotlin/cz/judas/jan/advent/year2023/Day14.kt
+++ src/main/kotlin/cz/judas/jan/advent/year2023/Day14.kt
@@ -1,32 +1,18 @@
 package cz.judas.jan.advent.year2023
 
+import cz.judas.jan.advent.Answer
 import cz.judas.jan.advent.InputData
 import cz.judas.jan.advent.Mutable2dArray
 import cz.judas.jan.advent.TwoDimensionalArray
 
 object Day14 {
+    @Answer("109833")
     fun part1(input: InputData): Int {
         val platform = TwoDimensionalArray.charsFromLines(input.lines())
-        val debug = Mutable2dArray(platform.numRows, platform.numColumns, '.')
-        return platform.columnIndices()
-            .sumOf { column ->
-                var total = 0
-                var current = platform.numRows
-                for (row in platform.rowIndices()) {
-                    val field = platform[row, column]
-                    if (field == '#') {
-                        debug.set(row, column, '#')
-                        current = platform.numRows - row - 1
-                    } else if (field == 'O') {
-                        debug.set(platform.numRows - current, column, 'O')
-                        total += current
-                        current -= 1
-                    }
-                }
-                total
-            }
+        return load(platform.tilt())
     }
 
+    @Answer("99875")
     fun part2(input: InputData): Int {
         var platform = TwoDimensionalArray.charsFromLines(input.lines())
         val history = mutableListOf<Int>()
@@ -38,7 +24,11 @@ object Day14 {
                 for (prevIndex in candidates) {
                     val length = history.size - prevIndex
                     if (length > 1 && prevIndex - length >= 0 && history[prevIndex - length] == tempResult) {
-                        if (history.subList(prevIndex - length, prevIndex) == history.subList(prevIndex, prevIndex + length)) {
+                        if (history.subList(prevIndex - length, prevIndex) == history.subList(
+                                prevIndex,
+                                prevIndex + length
+                            )
+                        ) {
                             return history[prevIndex - 1 + ((1000000000L - prevIndex) % length).toInt()]
                         }
                     }
@@ -81,16 +71,13 @@ object Day14 {
     }
 
     private fun load(platform: TwoDimensionalArray<Char>): Int {
-        return platform.columnIndices()
-            .sumOf { column ->
-                var load = 0
-                for (row in platform.rowIndices()) {
-                    val field = platform[row, column]
-                    if (field == 'O') {
-                        load += platform.numRows - row
-                    }
+        return platform.entries()
+            .sumOf { (coordinate, value) ->
+                if (value == 'O') {
+                    platform.numRows - coordinate.row
+                } else {
+                    0
                 }
-                load
             }
     }
 }
