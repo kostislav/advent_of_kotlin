6425ddfac3a71d677a13f730121329466dfc13fa
Jan Judas <jan@judas.cz>
2024-01-14 07:30:11 +0100

    cleanup 2023 day 7, but still with an ugly comparator

--- src/main/kotlin/cz/judas/jan/advent/year2023/Day7.kt
+++ src/main/kotlin/cz/judas/jan/advent/year2023/Day7.kt
@@ -4,20 +4,13 @@ import cz.judas.jan.advent.Answer
 import cz.judas.jan.advent.InputData
 import cz.judas.jan.advent.LexicographicalListComparator
 import cz.judas.jan.advent.Pattern
-import cz.judas.jan.advent.asMap
 import cz.judas.jan.advent.associateToIndex
+import cz.judas.jan.advent.histogram
 import cz.judas.jan.advent.mapIndexedFrom
-import cz.judas.jan.advent.multiSetOf
 import cz.judas.jan.advent.parserFor
-import cz.judas.jan.advent.replace
-import cz.judas.jan.advent.toMultiSet
-import cz.judas.jan.advent.year2023.Day7.CamelCardScoring.score
+import cz.judas.jan.advent.subList
 
 object Day7 {
-    private val comparator =
-        compareBy<Pair<Pair<Int, List<Int>>, Int>> { it.first.first }
-            .then(compareBy(LexicographicalListComparator.natural()) { it.first.second })
-
     private val parser = parserFor<Game>()
 
     @Answer("245794640")
@@ -27,7 +20,7 @@ object Day7 {
         return input.lines()
             .map { line ->
                 val game = parser.parse(line)
-                val score = score(game.hand)
+                val score = game.hand.histogram().values.sorted().reversed()
                 Pair(score, game.hand.map { labelIndexes.getValue(it) }) to game.bid
             }
             .let(::totalWinnings)
@@ -36,31 +29,28 @@ object Day7 {
     @Answer("247899149")
     fun part2(input: InputData): Int {
         val labelIndexes = listOf('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A').associateToIndex()
-        val nonJokers = labelIndexes.keys - 'J'
 
         return input.lines()
             .map { line ->
                 val game = parser.parse(line)
-                val score = metaScore(game.hand, nonJokers)
-                Pair(score, game.hand.map { labelIndexes.getValue(it) }) to game.bid
+                val score = game.hand.filterNot { it == 'J' }.histogram().values.sorted().reversed()
+                val numJokers = game.hand.count { it == 'J' }
+                val improvedScore = if (score.isNotEmpty()) {
+                    listOf(score[0] + numJokers) + score.subList(1)
+                } else {
+                    listOf(numJokers)
+                }
+                Pair(improvedScore, game.hand.map { labelIndexes.getValue(it) }) to game.bid
             }
             .let(::totalWinnings)
     }
 
-    private fun totalWinnings(games: List<Pair<Pair<Int, List<Int>>, Int>>): Int {
+    private fun totalWinnings(games: List<Pair<Pair<List<Int>, List<Int>>, Int>>): Int {
         return games
-            .sortedWith(comparator)
+            .sortedWith(HandComparator)
             .map { it.second }
             .mapIndexedFrom(1) { index, bid -> index * bid }
             .sum()
-    }
-
-    private fun metaScore(hand: List<Char>, otherLabels: Set<Char>): Int {
-        return if ('J' in hand) {
-            otherLabels.maxOf { score(hand.replace('J', it)) }
-        } else {
-            score(hand)
-        }
     }
 
     @Pattern("(.+) (\\d+)")
@@ -69,25 +59,12 @@ object Day7 {
         val bid: Int
     )
 
-    object CamelCardScoring {
-        private val fiveOfAKind = multiSetOf(5)
-        private val fourOfAKind = multiSetOf(4, 1)
-        private val fullHouse = multiSetOf(3, 2)
-        private val threeOfAKind = multiSetOf(3, 1, 1)
-        private val twoPairs = multiSetOf(2, 2, 1)
-        private val onePair = multiSetOf(2, 1, 1, 1)
+    object HandComparator: Comparator<Pair<Pair<List<Int>, List<Int>>, Int>> {
+        private val intListComparator = LexicographicalListComparator.natural<Int>()
 
-        fun score(hand: List<Char>): Int {
-            val distinctCardCounts = hand.toMultiSet().asMap().values.toMultiSet()
-            return when (distinctCardCounts) {
-                fiveOfAKind -> 7
-                fourOfAKind -> 6
-                fullHouse -> 5
-                threeOfAKind -> 4
-                twoPairs -> 3
-                onePair -> 2
-                else -> 1
-            }
+        override fun compare(o1: Pair<Pair<List<Int>, List<Int>>, Int>, o2: Pair<Pair<List<Int>, List<Int>>, Int>): Int {
+            val primary = intListComparator.compare(o1.first.first, o2.first.first)
+            return if (primary != 0) primary else intListComparator.compare(o1.first.second, o2.first.second)
         }
     }
 }
