f242b4b8cd4da009c0bf7d38d6b9515fff8b0e0d
Jan Judas <jan@judas.cz>
2024-05-03 20:00:49 +0200

    use method handle to save a few microseconds

--- src/main/kotlin/cz/judas/jan/advent/entrypoint.kt
+++ src/main/kotlin/cz/judas/jan/advent/entrypoint.kt
@@ -1,15 +1,20 @@
 package cz.judas.jan.advent
 
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.MethodType
+
 data class PuzzleResult(val answer: Any, val computationTime: Long)
 
 fun run(year: Int, day: Int, part: Int, submit: Boolean): PuzzleResult {
     val dayClass = Class.forName("cz.judas.jan.advent.year${year}.Day${day}")
     val partMethod = dayClass.getMethod("part${part}", InputData::class.java)
     val dayInstance = dayClass.getField("INSTANCE").get(null)
+    val lookup = MethodHandles.lookup()
+    val partMethodHandle = lookup.findVirtual(dayClass, partMethod.name, MethodType.methodType(partMethod.returnType, InputData::class.java))
     val inputFetcher = InputFetcher()
     val inputData = inputFetcher.get(year, day)
     val startTime = System.currentTimeMillis()
-    val answer = partMethod.invoke(dayInstance, inputData)
+    val answer = partMethodHandle.invoke(dayInstance, inputData)
     val endTime = System.currentTimeMillis()
     if (submit) {
         AdventOfCodeApi().submitAnswer(year, day, part, answer)
@@ -18,7 +23,7 @@ fun run(year: Int, day: Int, part: Int, submit: Boolean): PuzzleResult {
 }
 
 fun main() {
-    val result = run(year = 2015, day = 4, part = 2, submit = false)
+    val result = run(year = 2023, day = 1, part = 1, submit = false)
     println()
     println()
     println(result.answer)
